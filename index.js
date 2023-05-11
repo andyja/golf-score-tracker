71f975d25526543c6536dbdd9d0119734862e7d0
Andy Jackson <andypauljackson@gmail.com>
2023-05-11 11:51:48 +0100

    added functionality for multiple players

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let resetBtn = document.getElementById("reset-btn");
+let submitBtn = document.getElementById("num-players-submit-btn");
+let scoreBoard = document.getElementById("score-board");
 
-console.log(saveEl)
+function displayScoreBoardHtml(numPlayers) {
+  scoreBoard.innerHTML = '';
+
+  for (let i = 1; i <= numPlayers; i++) {
+    let scoreboardHtml = `
+      <div>
+        <h1>Player ${i} - Mini-golf counter:</h1>
+        <h2 id="count-el">0</h2>
+        <button onclick="increment()">INCREMENT</button>
+        <button onclick="save()">SAVE</button>
+        <p>Previous entries: <span id="save-el"></span></p>
+        <button onclick="resetPrevious())">RESET</button>
+      </div>
+    `;
+    scoreBoard.innerHTML += scoreboardHtml;
+  }
+}
+
+submitBtn.addEventListener('click', function() {
+  const numPlayersInput = document.getElementById("num-players-input");
+  const numPlayers = parseInt(numPlayersInput.value);
+  displayScoreBoardHtml(numPlayers);
+});
+
+
+
+
+
+
+let count = 0;
+let countStr = "";
+
+
+
 
 function increment() {
     count += 1;
     countEl.textContent = count;
 }
 function save() {
-    let countStr = count + " - "
+    countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0;
     count = 0;
+}
+function resetPrevious() {
+    countStr = "";
+    saveEl.textContent = countStr
 }
