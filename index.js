0452dbd3f24bf83569fd6e82e7d504f6d7540561
Andy Jackson <andypauljackson@gmail.com>
2023-05-15 08:44:48 +0100

    increment function added and working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let countEl = document.getElementById("count-el");
 let resetBtn = document.getElementById("reset-btn");
 let submitBtn = document.getElementById("num-players-submit-btn");
 let scoreBoard = document.getElementById("score-board");
+let count = 0;
 
 function displayScoreBoardHtml(numPlayers) {
   scoreBoard.innerHTML = '';
@@ -11,13 +12,14 @@ function displayScoreBoardHtml(numPlayers) {
     let scoreboardHtml = `
       <div>
         <h1>Player ${i} - Mini-golf counter:</h1>
-        <h2 id="count-el">0</h2>
-        <button onclick="increment()">INCREMENT</button>
-        <button onclick="save()">SAVE</button>
-        <p>Previous entries: <span id="save-el"></span></p>
-        <button onclick="resetPrevious())">RESET</button>
+        <h2 id="count-el-${i}">0</h2>
+        <button onclick="increment(${i})">INCREMENT</button>
+        <button class="save-btn" data-player="${i}">SAVE</button>
+        <p>Previous entries: <span id="save-el-${i}"></span></p>
+        <button onclick="resetPrevious(${i})">RESET</button>
       </div>
     `;
+
     scoreBoard.innerHTML += scoreboardHtml;
   }
 }
@@ -28,28 +30,48 @@ submitBtn.addEventListener('click', function() {
   displayScoreBoardHtml(numPlayers);
 });
 
+scoreBoard.addEventListener('click', function(event) {
+  if (event.target.classList.contains('save-btn')) {
+    const playerIndex = event.target.dataset.player;
+    save(playerIndex);
+  }
+});
 
+function save(playerIndex) {
+  console.log("Save clicked for Player", playerIndex);
 
-
-
-
-let count = 0;
-let countStr = "";
-
-
-
-
-function increment() {
-    count += 1;
-    countEl.textContent = count;
+  let saveEl = document.getElementById("save-el-${i}");  
 }
-function save() {
-    countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0;
-    count = 0;
+function increment(playerId) {
+  let countEl = document.getElementById(`count-el-${playerId}`);
+  let saveEl = document.getElementById(`save-el-${playerId}`);
+
+  count++;
+  let countStr = count + " - ";
+  countEl.innerText = countStr;
+
+  console.log(count);
 }
-function resetPrevious() {
-    countStr = "";
-    saveEl.textContent = countStr
-}
+
+
+
+
+
+
+// let count = 0;
+// let countStr = "";
+
+
+
+
+
+// function save() {
+//     countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0;
+//     count = 0;
+// }
+// function resetPrevious() {
+//     countStr = "";
+//     saveEl.textContent = countStr
+// }
